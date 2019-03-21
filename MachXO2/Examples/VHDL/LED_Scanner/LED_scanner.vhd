2240036057909f421eb1025448f500a073f84504
Manuel Caballero <aqueronteblog@gmail.com>
2019-03-21 20:39:43 +0000

    MACHO2X LED Scanner Example. Simulation is left the rest is completed and works as expected.

--- MachXO2/Examples/VHDL/LED_Scanner/LED_scanner.vhd
+++ MachXO2/Examples/VHDL/LED_Scanner/LED_scanner.vhd
@@ -17,6 +17,7 @@
 
 LIBRARY	IEEE;
 USE IEEE.std_logic_1164.all;
+USE ieee.numeric_std.all;
 
 LIBRARY machxo2;
 USE machxo2.all;
@@ -31,12 +32,13 @@ END LED_SCANNER_block;
 
 ARCHITECTURE LED of LED_SCANNER_block IS
 	SIGNAL clk			: STD_LOGIC;
-	signal shift_reg 	: STD_LOGIC_VECTOR (7 DOWNTO 0) := "10000000";
+	SIGNAL shift_reg 	: STD_LOGIC_VECTOR (7 DOWNTO 0) := "10000000";
+	SIGNAL state		: UNSIGNED(3 DOWNTO 0)			:= (OTHERS => '0');
 	
 	--internal oscillator
    COMPONENT OSCH
       GENERIC (
-            NOM_FREQ: STRING := "2.08"
+				NOM_FREQ: STRING := "2.08"
 			  );
       PORT( 
             STDBY    : IN  STD_LOGIC;
@@ -46,24 +48,48 @@ ARCHITECTURE LED of LED_SCANNER_block IS
    END COMPONENT;
 BEGIN
 	--internal oscillator
-   OSCInst0: OSCH
+	OSCInst0: OSCH
       GENERIC MAP (NOM_FREQ  => "2.08")
       PORT MAP (STDBY => '0', OSC => clk, SEDSTDBY => OPEN);
-   PROCESS(clk, reset)
-      VARIABLE count :   INTEGER RANGE 0 TO 416_000;
-   BEGIN
-      IF (reset = '1') THEN
-		count 		:=	0;
-		shift_reg   <=	"10000000";					-- LED1 on, rest off 
-	  ELSIF(clk'EVENT AND clk = '1') THEN
-         IF(count < 416_000) THEN
-            count := count + 1;
-         ELSE
-            count := 0;				
-         END IF;
-      END IF;
-   END PROCESS;
-  
-   leds_scanner   <= shift_reg;						-- Update LEDs 
+
+-- Generate a delay ( 208000 * ( 1 / 2.08MHz ) = 100ms ) and analyze the reset pin	  
+	LED_Divider:	PROCESS(clk, reset)
+		VARIABLE count :   INTEGER RANGE 0 TO 208_000;
+		BEGIN
+			IF (reset = '1') THEN
+				count 	:=	0;
+				state   <=	TO_UNSIGNED(1,4);				-- state = 1 
+			ELSIF(clk'EVENT AND clk = '1') THEN
+				IF(count < 208_000) THEN
+					count := count + 1;
+					state <= state;
+				ELSE
+					count := 0;	
+					IF(state = TO_UNSIGNED(14,4)) THEN
+						state   <=	TO_UNSIGNED(1,4);		-- state = 1
+					ELSE
+						state <= state + 1;					-- update the state
+					END IF;
+				END IF;
+			END IF;
+	END PROCESS;
    
-END LED;
+-- Analyze the state    
+	LED_State:	PROCESS(state)
+		BEGIN
+			CASE TO_INTEGER(state) IS
+				WHEN 1		=> shift_reg <= "10000000";
+				WHEN 2|14	=> shift_reg <= "01000000";
+				WHEN 3|13	=> shift_reg <= "00100000";
+				WHEN 4|12	=> shift_reg <= "00010000";
+				WHEN 5|11	=> shift_reg <= "00001000";
+				WHEN 6|10	=> shift_reg <= "00000100";
+				WHEN 7|9	=> shift_reg <= "00000010";
+				WHEN 8		=> shift_reg <= "00000001";
+				WHEN OTHERS	=> shift_reg <= "00000000";
+			END CASE;
+	END PROCESS;
+  
+	leds_scanner   <= NOT shift_reg;						-- Update LEDs 
+   
+END ARCHITECTURE LED;
